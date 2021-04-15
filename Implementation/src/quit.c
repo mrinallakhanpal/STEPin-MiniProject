513f056fe9c99b1634fc25923e80997bb59073a8
mrinallakhanpal@gmail.com <mrinallakhanpal@gmail.com>
2021-04-15 14:22:05 +0530

    frst

---
+++ Implementation/src/quit.c
@@ -0,0 +1,13 @@
+#include "func.h"
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void quit()
+{
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n __________________________________________________________________________________________________");
+    printf("\n\n\t\t\t* * * * THANK YOU FOR COMMING HERE , HAVE A GREAT DAY ! * * * *");
+    printf("\n __________________________________________________________________________________________________\n\n\n\n\n");
+    return "success";
+}
